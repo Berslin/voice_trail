aee1ebfeb5ec6e8a54b6028c62ae8feb1b72ef8e
Berslin J <berslin.j@frankmax.digital>
2022-08-18 16:53:09 +0530

    added a folder

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="midi2voice",
+    version="1.0.4",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests", "music21", "Pyphen"],
+    python_requires='>=3.6',
+)
